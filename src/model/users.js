b639e7a7841e0c3f8f4712c8a1135c6cdf49e655
thomascostta <thomas.jeffcosta@gmail.com>
2022-02-07 16:20:40 -0300

    folder setting

---
+++ src/model/users.js
@@ -0,0 +1,30 @@
+export default Users = [
+    {
+        id: 1, 
+        email: 'user1@email.com',
+        username: 'user1', 
+        password: 'password', 
+        userToken: 'token123'
+    },
+    {
+        id: 2, 
+        email: 'user2@email.com',
+        username: 'user2', 
+        password: 'pass1234', 
+        userToken: 'token12345'
+    },
+    {
+        id: 3, 
+        email: 'testuser@email.com',
+        username: 'testuser', 
+        password: 'testpass', 
+        userToken: 'testtoken'
+    },
+    {
+        id: 4,
+        email: 'teste@gmail.com',
+        username: 'Thomas',
+        password: 'teste',
+        userToken: 'teste_Token',
+    }
+];
