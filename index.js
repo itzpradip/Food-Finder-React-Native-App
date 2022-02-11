3f037b03aad1e77559b5a90607ef9f9de12403a0
thomascostta <thomas.jeffcosta@gmail.com>
2022-02-11 14:37:24 -0300

    settings Provider

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
  */
 import React from 'react';
 import { Provider } from 'react-redux';
-import { Store } from './src/store/index.js';
+import store from './src/store';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
 const Root = () => (
-    <Provider store={Store}>
+    <Provider store={store}>
         <App />
     </Provider>
 )
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Root);
